3632077b5a76822a16e77414a033a2bed4e5cba4
Jakub Kulik <jakub.kulik1@o2.pl>
2024-03-02 22:34:10 +0100

    intervalClear

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -4,19 +4,23 @@ import "./style.css";
 const Clock = () => {
     const [date, setDate] = useState(new Date);
     useEffect(() => {
-        setInterval(() => { setDate(new Date) }, 1000);
+        const intervalId = setInterval(() => { setDate(new Date) }, 1000);
+        setTimeout(() => { clearInterval(intervalId) }, 1000);
     });
+
+    const actualDate = date.toLocaleString(undefined, {
+        weekday: "long",
+        day: "2-digit",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    })
+
     return (
         <div className="clock">
             Aktualna data: {" "}
-            {date.toLocaleString(undefined, {
-                weekday: "long",
-                day: "2-digit",
-                month: "long",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-            })}
+            {actualDate}
         </div>
     )
 };
